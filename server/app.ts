76ea16101d5b3a3cec8b07e59305b1ad30146e38
Vasilkou Stanislau <stamlymia98@gmail.com>
2018-11-02 04:43:30 +0300

    fixed CORS issue

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,11 @@ let users = JSON.parse(fs.readFileSync('./server/users.json'), 'utf-8');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/users', (req, res) => res.send(users));
 
